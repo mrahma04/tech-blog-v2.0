5bfcfb65e44efc6cadf7601c6d1a2a88e7b7ab8a
Mohammad Rahman <mbrahman@gmail.com>
2022-04-18 13:14:50 -0400

    created single-post links for individiual posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router()
-const { Post } = require('../models')
+const { Post, User } = require('../models')
+const sequelize = require('../config/connection')
 
 router.get('/', async (req, res) => {
     console.log(req.session)
     try {
         const dbPostData = await Post.findAll({
-            attributes: ['title', 'post_content']
+            attributes: ['id', 'title', 'post_content', [sequelize.fn('DATE_FORMAT', sequelize.col('created_at'), '%d/%m/%Y'), 'created_at']],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
         })
         // get method serializes the object
         // turns the JavaScript object into a JSON string that's fed into the handlebars templates
@@ -14,6 +21,34 @@ router.get('/', async (req, res) => {
         res.render('homepage', { posts })
     } catch (err) {
         console.error(err)
+        res.status(500).json(err)
+    }
+})
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const dbPostData = await Post.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['title', 'post_content', [sequelize.fn('DATE_FORMAT', sequelize.col('created_at'), '%d/%m/%Y'), 'created_at']],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+        // res.status(200).json(dbPostData)
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' })
+            return
+        }
+        const post = dbPostData.get({ plain: true })
+        console.log(post)
+        res.render('single-post', { post })
+    } catch (err) {
+        console.log(err)
         res.status(500).json(err)
     }
 })
