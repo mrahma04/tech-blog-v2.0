ae6ab3d3f56761f8534239640855b0e7f4b59cf5
Mohammad Rahman <mbrahman@gmail.com>
2022-04-20 13:46:22 -0400

    added authguard to dashboard route

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const sequelize = require('../config/connection')
 const { Post, User, Comment } = require('../models')
+const withAuth = require('../utils/auth')
 
-router.get('/', async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
     try {
         console.log(req.session);
         console.log('======================');
